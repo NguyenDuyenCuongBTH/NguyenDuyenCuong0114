7fa2e071dce3a4e39c48a42d9d0cad4b30bbd9cc
NDC114 <duyencuongmtpvip2001@gmail.com>
2023-06-21 09:25:19 +0700

    commit note

--- NguyenDuyenCuong0114/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace NguyenDuyenCuong0114.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
